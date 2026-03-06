00adbb5d62265366eb9bd238834211507b16228f
Jakob Hellermann <hellermann@sipgate.de>
2026-03-06 12:34:54 +0100

    nix: remove flake utils

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -12,80 +11,76 @@
       self,
       nixpkgs,
       crane,
-      flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.mkLib pkgs;
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      eachSystem = nixpkgs.lib.genAttrs supportedSystems;
 
-        # Common arguments for all builds
-        commonArgs = {
-          src = craneLib.cleanCargoSource ./.;
-          strictDeps = true;
+      mkFor =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          craneLib = crane.mkLib pkgs;
+          commonArgs = {
+            src = craneLib.cleanCargoSource ./.;
+            strictDeps = true;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { pname = "zmqtt2prom-rs-deps"; });
+        in
+        {
+          inherit pkgs craneLib;
+          zmqtt2prom-rs = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+          zmqtt2prom-rs-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+          zmqtt2prom-rs-test = craneLib.cargoTest (commonArgs // { inherit cargoArtifacts; });
         };
 
-        # Build only dependencies for caching
-        cargoArtifacts = craneLib.buildDepsOnly (
-          commonArgs
-          // {
-            pname = "zmqtt2prom-rs-deps";
-          }
-        );
+      forAllSystems = eachSystem mkFor;
+    in
+    {
+      packages = eachSystem (system: {
+        default = forAllSystems.${system}.zmqtt2prom-rs;
+        zmqtt2prom-rs = forAllSystems.${system}.zmqtt2prom-rs;
+      });
 
-        # Build the actual package
-        zmqtt2prom-rs = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
-
-        # Clippy check
-        zmqtt2prom-rs-clippy = craneLib.cargoClippy (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          }
-        );
-
-        # Run tests
-        zmqtt2prom-rs-test = craneLib.cargoTest (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
-      in
-      {
-        packages = {
-          default = zmqtt2prom-rs;
-          zmqtt2prom-rs = zmqtt2prom-rs;
-        };
-
-        apps.default = {
+      apps = eachSystem (system: {
+        default = {
           type = "app";
-          program = "${zmqtt2prom-rs}/bin/zmqtt2prom";
+          program = "${forAllSystems.${system}.zmqtt2prom-rs}/bin/zmqtt2prom";
         };
+      });
 
-        checks = {
-          inherit zmqtt2prom-rs zmqtt2prom-rs-clippy zmqtt2prom-rs-test;
-        };
+      checks = eachSystem (system: {
+        inherit (forAllSystems.${system}) zmqtt2prom-rs zmqtt2prom-rs-clippy zmqtt2prom-rs-test;
+      });
 
-        devShells.default = craneLib.devShell {
-          checks = self.checks.${system};
-
-          inputsFrom = [ zmqtt2prom-rs ];
-
-          packages = with pkgs; [
-            rust-analyzer
-            cargo-watch
-            cargo-edit
-          ];
-        };
-      }
-    );
+      devShells = eachSystem (
+        system:
+        let
+          f = forAllSystems.${system};
+        in
+        {
+          default = f.craneLib.devShell {
+            checks = self.checks.${system};
+            inputsFrom = [ f.zmqtt2prom-rs ];
+            packages = with f.pkgs; [
+              rust-analyzer
+              cargo-watch
+              cargo-edit
+            ];
+          };
+        }
+      );
+    };
 }
